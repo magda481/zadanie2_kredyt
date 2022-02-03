86c92a6f869179049e3bc849db983e7ff318cb2c
Magdalena Stan <stan.magdalena481@gmail.com>
2022-02-03 16:49:22 +0100

    dodanie rozwiązania zadania o wysokości rat kredytu z użyciem pliku pomocniczego i powtarzaniem poleceń

---
+++ lista.py
@@ -0,0 +1,173 @@
+kwota_pozyczki = round(float(input()),2)
+kwota_raty = round(float(input()),2)
+oprocentowanie = float(input())
+
+tmp = "Twoja pozostała kwota kredytu to {} złotych, to {} mniej niż w poprzednim miesiącu"
+tmp_2 = "Kwota początkowa kredytu to {} złotych, rata wynosi {} złotych, a oprocentowanie to {} %"
+
+print(tmp_2.format(kwota_pozyczki,kwota_raty,oprocentowanie))
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
+
+
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
+
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
+
+
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
+
+
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
+
+
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
+
+
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
+
+
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
+
+
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
+
+
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
+
+
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
+
+
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
+
+
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
+
+
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
+
+
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
+
+
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
+
+
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
+
+
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
+
+
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
+
+
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
+
+
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
+
+
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
+
+
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
+
+
+inflacja = float(input())
+pozostało = round((1+((inflacja+oprocentowanie)/1200))*kwota_pozyczki-200,2)
+roznica = round(kwota_pozyczki-pozostało,2)
+print(tmp.format(pozostało,roznica))
+kwota_pozyczki = pozostało
